45da64200b35a88a5583672e36eaa416b33251da
Nurtore7 <nurtore.suinbay@icloud.com>
2026-02-17 12:36:13 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

--- Practice3/inheritance/inheritance_basics.py
+++ Practice3/inheritance/inheritance_basics.py
@@ -1,9 +1,16 @@
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
-  def printname(self):
-    print(self.firstname, self.lastname)
-#Use the Person class to create an object, and then execute the printname method:
-x = Person("John", "Doe")
-x.printname()
+# Родительский класс
+class Vehicle:
+    def move(self):
+        print("The vehicle is moving")
+
+
+# Дочерний класс
+class Car(Vehicle):
+    def honk(self):
+        print("Beep beep!")
+
+
+# Создаем объект
+c = Car()
+c.move()   # метод родителя
+c.honk()   # метод дочернего класса
